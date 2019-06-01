d038f3fbb98c57bac58d1e7483c9e642e93d8598
mishoryu <22555149+mishoryu@users.noreply.github.com>
2019-06-01 17:28:25 +1000

    Added option to authenticate twitter using app authorization
    If no API keys are passed to Authenticate.twitter then it will try to create a token using rtweets rstats2twitter app authorization by user via a web browser.

--- R/Authenticate.twitter.R
+++ R/Authenticate.twitter.R
@@ -1,6 +1,7 @@
 #' @title Twitter API authentication
 #' 
-#' @description Twitter authentication uses OAuth and requires Application-user authentication API keys as described here: 
+#' @description Twitter authentication uses OAuth and either requires authorization of the rtweet package rstats2twitter
+#' client app by a registered twitter user or twitter app developer API keys as described here: 
 #' \url{https://developer.twitter.com/en/docs/basics/authentication/overview/oauth}.
 #' 
 #' @param socialmedia Character string. Identifier for social media API to authenticate, set to \code{"twitter"}.
@@ -16,7 +17,11 @@
 #' 
 #' @examples
 #' \dontrun{
-#' # twitter authentication with api keys
+#' # twitter authentication via user authorization of rstats2twitter app
+#' # will open a web browser to twitter prompting the user to log in and authorize the app
+#' twitterAuth <- Authenticate("twitter")
+#' 
+#' # twitter authentication with app developer api keys
 #' myKeys <- list(appName = "vosonSML", apiKey = "xxxxxxxxxxxx",
 #'   apiSecret = "xxxxxxxxxxxx", accessToken = "xxxxxxxxxxxx",
 #'   accessTokenSecret = "xxxxxxxxxxxx")
@@ -28,30 +33,30 @@
 #' 
 #' @export
 Authenticate.twitter <- function(socialmedia, appName, apiKey, apiSecret, accessToken, accessTokenSecret, ...) {
+  credential <- list(socialmedia = "twitter", auth = NULL)
+  class(credential) <- append(class(credential), c("credential", "twitter"))   
+  
+  if (missing(apiKey) & missing(apiSecret) & missing(accessToken) & missing(accessTokenSecret)) {
+    credential$auth <- rtweet:::rstats2twitter_client()
+    
+    return(credential)
+  }
+  
+  if (missing(appName)) {
+    stop("Missing twitter app name.", call. = FALSE)
+  }
   
   if (missing(apiKey) || missing(apiSecret) || missing(accessToken) || missing(accessTokenSecret)) {
     stop("Missing one or more twitter API keys.", call. = FALSE)
   }
   
-  if (missing(appName)) {
-    appName <- "vosonSML-twitter"
-  }
-  
-  twitter_oauth <- NULL
-  token_file_name <- ".twitter-oauth"
-  
-  credential <- list(socialmedia = "twitter", auth = NULL)
-  class(credential) <- append(class(credential), c("credential", "twitter"))
-  
-  twitter_oauth <- rtweet::create_token(
+  credential$auth <- rtweet::create_token(
     app = appName,
     consumer_key = apiKey,
     consumer_secret = apiSecret,
     access_token = accessToken,
     access_secret = accessTokenSecret,
     set_renv = FALSE)
-  
-  credential$auth <- twitter_oauth
   
   return(credential)
 }
