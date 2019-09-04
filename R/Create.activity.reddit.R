d2ec9318b2d4bd5c1ecd3721e46fe9bea070f8c5
mishoryu <22555149+mishoryu@users.noreply.github.com>
2019-09-05 07:01:08 +1000

    Added reddit activity network (#18)

--- R/Create.activity.reddit.R
+++ R/Create.activity.reddit.R
@@ -1,16 +1,88 @@
-# @title Create reddit activity network
-# 
-# @export
+#' @title Create reddit activity network
+#'
+#' @description Creates a reddit activity network from subreddit thread comments. Nodes are comments and initial thread
+#' posts, edges form the discussion structure and signify to which comment or post a comment has been made to.
+#' 
+#' @param datasource Collected social media data with \code{"datasource"} and \code{"reddit"} class names.
+#' @param type Character string. Type of network to be created, set to \code{"activity"}.
+#' @param writeToFile Logical. Save network data to a file in the current working directory. Default is \code{FALSE}.
+#' @param verbose Logical. Output additional information about the network creation. Default is \code{TRUE}.
+#' @param ... Additional parameters passed to function. Not used in this method.
+#'
+#' @return Named list containing generated network as igraph object \code{$graph}.
+#' 
+#' @examples
+#' \dontrun{
+#' # create a reddit activity network graph
+#' activityNetwork <- redditData %>% 
+#'   Create("activity", writeToFile = TRUE)
+#' 
+#' # igraph object
+#' # activityNetwork$graph
+#' }
+#' 
+#' @export
 Create.activity.reddit <- function(datasource, type, writeToFile = FALSE, verbose = TRUE, ...) {
   df <- tibble::as_tibble(datasource) 
   
   df_stats <- networkStats(NULL, "collected reddit comments", nrow(df))
   
-  # cat("Generating youtube activity network...\n")
-  # flush.console()
+  cat("Generating reddit activity network...\n")
+  flush.console()
   
-  # edges
-  # df_relations <- df %>% 
-  #   dplyr::select(.data$id, .data$subreddit, .data$thread_id, .data$structure, .data$user) %>% 
-  #   dplyr::rename("comment_id" = .data$id)
+  # would be better with the unique comment fullname ids
+  # comment id format <thread_id>.<structure>
+  df %<>% dplyr::mutate(comment_id = paste0(.data$thread_id, ".", .data$structure))
+  
+  # edges  
+  df_relations <- df %>%
+    dplyr::rename("from" = .data$comment_id) %>%
+    dplyr::mutate(to = ifelse(!grepl("_", .data$structure), paste0(.data$thread_id, ".0"), 
+                              gsub("_\\d+$", "", .data$from))) %>%
+    dplyr::mutate(edge_type = "comment") %>%
+    dplyr::select(.data$from, .data$to, .data$edge_type)
+  
+  # nodes
+  df_nodes <- df %>% dplyr::select(.data$comment_id, .data$comm_date, .data$subreddit, .data$user) %>%
+    dplyr::rename("id" = .data$comment_id, "date" = .data$comm_date) %>%
+    dplyr::mutate(node_type = "comment")
+  
+  # add thread posts to nodes
+  thread_ids <- df %>% dplyr::select(.data$subreddit, .data$author, .data$thread_id, .data$post_date) %>%
+    dplyr::mutate(id = paste0(.data$thread_id, ".0")) %>% 
+    dplyr::distinct(.data$subreddit, .data$id, .keep_all = TRUE) %>%
+    dplyr::rename("user" = .data$author, "date" = .data$post_date) %>%
+    dplyr::select(.data$id, .data$date, .data$subreddit, .data$user) %>%
+    dplyr::mutate(node_type = "thread")
+  
+  df_nodes <- dplyr::bind_rows(df_nodes, dplyr::anti_join(thread_ids, df_nodes, by = c("id", "subreddit")))
+  
+  df_stats <- networkStats(df_stats, "threads", nrow(thread_ids))
+  df_stats <- networkStats(df_stats, "comments", nrow(df))
+  df_stats <- networkStats(df_stats, "network nodes", nrow(df_nodes))
+  df_stats <- networkStats(df_stats, "network edges", nrow(df_relations))  
+  
+  # print stats
+  if (verbose) { networkStats(df_stats, print = TRUE) }
+  
+  g <- igraph::graph_from_data_frame(d = df_relations, directed = TRUE, vertices = df_nodes)
+  
+  V(g)$label <- ifelse(!is.na(V(g)$user), paste0(V(g)$name, " (", V(g)$user, ")"), V(g)$name)
+  
+  g <- igraph::set_graph_attr(g, "type", "reddit")
+  
+  if (writeToFile) { writeOutputFile(g, "graphml", "RedditActivityNetwork") }
+  
+  cat("Done.\n")
+  flush.console()
+  
+  func_output <- list(
+    "edges" = df_relations,
+    "nodes" = df_nodes,
+    "graph" = g
+  )
+  
+  class(func_output) <- append(class(func_output), c("network", "activity", "reddit"))
+  
+  return(func_output)  
 }
