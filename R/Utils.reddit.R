93605c019a7a5c9d6ad71eb92d3624572ab7520b
mishoryu <22555149+mishoryu@users.noreply.github.com>
2019-12-08 20:13:27 +1100

    Fixed problem with comment id
    Function was returning comment id's for comments that had been filtered out of the threads because of missing authors.

--- R/Utils.reddit.R
+++ R/Utils.reddit.R
@@ -27,24 +27,31 @@ FullCleanText <- function(sentences) {
 
 # modified RedditExtractoR reddit_content function
 # original author @ivan-rivera
-mod_reddit_content = function(URL, wait_time = 2) {
+mod_reddit_content = function(URL, wait_time = 2, data = NULL) {
   
   if(is.null(URL) | length(URL)==0 | !is.character(URL)) { stop("invalid URL parameter") }
   
   # setting up a function for extraction of comment specific information:
-  GetAttribute  = function(node,feature){
+  GetAttribute  = function(node, feature){
+    filter = is.null(node$data[["author"]])
+    
     Attribute   = node$data[[feature]]
     replies     = node$data$replies
     reply.nodes = if (is.list(replies)) replies$data$children else NULL
-    return(list(Attribute, lapply(reply.nodes,function(x){GetAttribute(x,feature)})))  
+    
+    return(list(Attribute, 
+                lapply(reply.nodes, function(x) { GetAttribute(x, feature) })))  
   }
   
-  get.structure = function(node, depth=0) {
-    if(is.null(node)) {return(list())}
-    filter     = is.null(node$data$author)
-    replies     = node$data$replies
+  get.structure = function(node, depth = 0) {
+    if (is.null(node)) { return(list()) }
+    
+    filter = is.null(node$data$author)
+    replies = node$data$replies
     reply.nodes = if (is.list(replies)) replies$data$children else NULL
-    return(list(paste0(filter," ",depth), lapply(1:length(reply.nodes), function(x) get.structure(reply.nodes[[x]], paste0(depth, "_", x)))))
+    
+    return(list(paste0(filter, " ", depth), 
+                lapply(1:length(reply.nodes), function(x) get.structure(reply.nodes[[x]], paste0(depth, "_", x)))))
   }
   
   # setting up the data frame
@@ -70,12 +77,21 @@ mod_reddit_content = function(URL, wait_time = 2) {
                             domain           = character(),
                             URL              = character())
   
+  rem_indexes <- function(index_list, data_list) { 
+    if (length(index_list) == 0) {
+      return(data_list)
+    } 
+    data_list[-index_list] 
+  }
+  
   for(i in seq(URL)){
     
     if(!grepl("^https?://(.*)",URL[i])) URL[i] = paste0("https://www.",gsub("^.*(reddit\\..*$)","\\1",URL[i]))
     if(!grepl("\\?ref=search_posts$",URL[i])) URL[i] = paste0(gsub("/$","",URL[i]),"/?ref=search_posts")
     
-    X = paste0(gsub("\\?ref=search_posts$","",URL[i]),".json?limit=500&enc=utf-8") # 500 is the maximum
+    X = paste0(gsub("\\?ref=search_posts$","",URL[i]),".json?limit=500&enc=utf-8") # 500 is the maximum &enc=utf-8
+    
+    if (is.null(data)) {
     raw_data = tryCatch(RJSONIO::fromJSON(readLines(X, warn = FALSE)),error = function(e) NULL)
     
     # try again if it fails
@@ -83,6 +99,11 @@ mod_reddit_content = function(URL, wait_time = 2) {
       Sys.sleep(min(1, wait_time))
       raw_data = tryCatch(RJSONIO::fromJSON(readLines(X, warn = FALSE)), error = function(e) NULL)
     }
+    }
+    
+    if (!is.null(data)) {
+      raw_data <- data
+    } 
     
     if(is.null(raw_data) == FALSE){
       
@@ -92,38 +113,41 @@ mod_reddit_content = function(URL, wait_time = 2) {
       
       if(min(length(meta.node),length(main.node)) > 0){
         
-        structure     = unlist(lapply(1:length(main.node), function(x) get.structure(main.node[[x]], x)))
+        structure <- unlist(lapply(1:length(main.node), function(x) get.structure(main.node[[x]], x)))
         
-        TEMP          =          data.frame(id               = NA,
-                                            structure        = gsub("FALSE ","",structure[!grepl("TRUE",structure)]),
-                                            post_date        = as.character(
-                                              as_datetime(as.numeric(meta.node$created_utc), tz = "UTC")
-                                            ),
-                                            post_date_unix   = as.numeric(meta.node$created_utc),
-                                            comm_id          = unlist(lapply(main.node, function(x){ GetAttribute(x, "id")})),
-                                            comm_date        = as.character(
-                                              as_datetime(as.numeric(unlist(lapply(main.node, function(x) { GetAttribute(x, "created_utc") }))), tz = "UTC")
-                                            ),
-                                            comm_date_unix   = as.numeric(unlist(lapply(main.node, function(x) { GetAttribute(x, "created_utc")}))),                                            
-                                            num_comments     = meta.node$num_comments,
-                                            subreddit        = ifelse(is.null(meta.node$subreddit), "UNKNOWN", meta.node$subreddit),
-                                            upvote_prop      = meta.node$upvote_ratio,
-                                            post_score       = meta.node$score,
-                                            author           = meta.node$author,
-                                            user             = unlist(lapply(main.node, function(x){ GetAttribute(x, "author")})),
-                                            comment_score    = unlist(lapply(main.node, function(x){ GetAttribute(x, "score")})),
-                                            controversiality = unlist(lapply(main.node, function(x){ GetAttribute(x, "controversiality")})),
-                                            comment          = unlist(lapply(main.node, function(x){ GetAttribute(x, "body")})),
-                                            title            = meta.node$title,
-                                            post_text        = meta.node$selftext,
-                                            link             = meta.node$url,
-                                            domain           = meta.node$domain,
-                                            URL              = URL[i],
-                                            stringsAsFactors = FALSE)
+        no_auth_inds <- which(grepl("TRUE", structure))
+        
+        TEMP = data.frame(id               = NA,
+                          structure        = gsub("FALSE " , "", structure[!grepl("TRUE", structure)]),
+                          post_date        = as.character(
+                            as_datetime(as.numeric(meta.node$created_utc), tz = "UTC")
+                          ),
+                          post_date_unix   = as.numeric(meta.node$created_utc),
+                          comm_id          = rem_indexes(no_auth_inds, 
+                                                         unlist(lapply(main.node, function(x) { GetAttribute(x, "id") }))),
+                          comm_date        = as.character(
+                            as_datetime(as.numeric(unlist(lapply(main.node, function(x) { GetAttribute(x, "created_utc") }))), tz = "UTC")
+                          ),
+                          comm_date_unix   = as.numeric(unlist(lapply(main.node, function(x) { GetAttribute(x, "created_utc") }))),                                            
+                          num_comments     = meta.node$num_comments,
+                          subreddit        = ifelse(is.null(meta.node$subreddit), "UNKNOWN", meta.node$subreddit),
+                          upvote_prop      = meta.node$upvote_ratio,
+                          post_score       = meta.node$score,
+                          author           = meta.node$author,
+                          user             = unlist(lapply(main.node, function(x) { GetAttribute(x, "author") })),
+                          comment_score    = unlist(lapply(main.node, function(x) { GetAttribute(x, "score") })),
+                          controversiality = unlist(lapply(main.node, function(x) { GetAttribute(x, "controversiality") })),
+                          comment          = unlist(lapply(main.node, function(x) { GetAttribute(x, "body") })),
+                          title            = meta.node$title,
+                          post_text        = meta.node$selftext,
+                          link             = meta.node$url,
+                          domain           = meta.node$domain,
+                          URL              = URL[i],
+                          stringsAsFactors = FALSE)
         
         TEMP$id = 1:nrow(TEMP)
         
-        if (dim(TEMP)[1]>0 & dim(TEMP)[2]>0) data_extract = rbind(TEMP,data_extract)
+        if (dim(TEMP)[1]>0 & dim(TEMP)[2]>0) data_extract = rbind(TEMP, data_extract)
         else print(paste("missed", i, ":", URL[i]))
       }
     }
